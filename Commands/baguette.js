5153927fef00717fcb0fbbad6609a13f4cad0510
GTink911 <42620721+GTink911@users.noreply.github.com>
2020-11-23 11:26:12 -0800

    Reorganized file structure
    Also made changes that allow for easier future development.

---
+++ Commands/baguette.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'baguette',
+	description: 'Displays a picture of a Baguette. <3',
+	execute(message, args) {
+		message.channel.send('https://cdn.discordapp.com/attachments/779186447969943564/779201107439255582/b_2.png');
+	},
+};
---
+++ Commands/snak.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'snak',
+	description: 'Displays a wonderful picure of a candy bar',
+	execute(message, args) {
+		message.channel.send('https://cdn.discordapp.com/attachments/779214584502288395/779227621794906182/Z.png');
+	},
+};
